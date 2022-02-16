3f10c897306369d81f602a87c637c62d9737c8d7
Brendan Posehn <brendan@daoai.ca>
2022-02-16 09:14:49 -0800

    Make naming convention uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from CrawlScrapeScoreService import CrawlScrapeScoreService
+from crawl_scrape_score_service import CrawlScrapeScoreService
 
 if __name__ == '__main__':
     srv = CrawlScrapeScoreService()
     srv.run()
-
